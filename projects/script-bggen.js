eddf975d902ac68a2f3d29ddc3c2f3b6ecc90edf
KatyaDru <kdrusakov@gmail.com>
2022-08-26 17:10:25 +0300

    ordered css

--- projects/script-bggen.js
+++ projects/script-bggen.js
@@ -1,12 +1,11 @@
-const bgGen = document.querySelector(".bgGen");
+const backgroundGenerator = document.querySelector(".backgroundGenerator");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
 
 function setGradient() {
 	body.style.background = "linear-gradient(" + color1.value + ", " + color2.value + ")";
-
-	bgGen.textContent = body.style.background + ";";
+	backgroundGenerator.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
